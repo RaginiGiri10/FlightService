330c14e43ed4fdba2ca78bb5a237e5335210e158
Nitesh <niteshbhushan95@gmail.com>
2021-01-03 19:24:41 +0530

    Committed

--- src/main/java/com/bhushan/tx/PdfGeneration/FlightAcknowledgementPdf.java
+++ src/main/java/com/bhushan/tx/PdfGeneration/FlightAcknowledgementPdf.java
@@ -2,7 +2,6 @@ package com.bhushan.tx.PdfGeneration;
 
 import java.io.FileOutputStream;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.bhushan.tx.dto.FlightBookingAcknowledgement;
